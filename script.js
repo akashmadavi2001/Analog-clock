1761d8d30cef3881c7257a5338a07da978d3e1cc
Akash Madavi <akashmadavi2001@gmail.com>
2025-03-30 09:24:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-var hr = 0;
-var min = 0;
-var sec = 0;
 setInterval(() => {
+    var [hr,min,sec] = [0,0,0];
     var data = new Date();
-    hr = data.getHours() * 30 + Math.round(min / 12);
     sec = data.getSeconds() * 6;
     min = data.getMinutes() * 6;
+    hr = data.getHours() * 30 + Math.round(min / 12);
     document.querySelector('.hour').style.transform = 'rotate(' + hr + 'deg)';
     document.querySelector('.sec').style.transform = 'rotate(' + sec + 'deg)';
     document.querySelector('.min').style.transform = 'rotate(' + min + 'deg)';
